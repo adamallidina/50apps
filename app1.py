6b19ba97fc01712200dd717a356f49362c0bcd3a
Rob <rob.s.brit@gmail.com>
2012-01-22 14:02:15 -0500

    Added complex part.

--- app1.py
+++ app1.py
@@ -3,22 +3,47 @@
 #
 # Unfortunately I didn't have time to do the complex version :(
 
-import sys, httplib, re
+import sys, httplib, re, getopt
 
 from BeautifulSoup import BeautifulSoup, HTMLParseError
 
 # don't do anything unless we provide all the proper arguments
 if len(sys.argv) < 4:
   print "Not enough arguments."
-  print "Usage: python app1.py <url> <depth> <search_text>"
+  print "Usage: python app1.py [--blacklist=<file>] --depth=<depth> --url=<url> [--url-filter=<filter>] <search_text> [<more search text> ...]"
   sys.exit()
 
-# the URL we want to search
-url         = sys.argv[1]
-# the depth we want to search until
-depth       = int(sys.argv[2])
-# what are we searching for?
-search_text = sys.argv[3]
+opts, args = getopt.getopt(sys.argv[1:], "b:d:u:", ["blacklist=", "depth=", "url=", "url-filter="])
+
+# a list of possible blacklisted files
+blacklist = []
+# the URL to search
+url   = None
+# how far should we follow links?
+depth = 5
+# what should we search for?
+search_texts = args
+# should we filter the URLs?
+url_filter = None
+
+# parse out the options
+for opt, value in opts:
+  if opt == "--blacklist":
+    f = open(value)
+    blacklist = f.readlines()
+    f.close()
+  elif opt == "--url":
+    url = value
+  elif opt == "--depth":
+    depth = int(value)
+  elif opt == "--url-filter":
+    url_filter = value
+  else:
+    print "Unrecognized option: \"%s\"" % opt
+
+if url == None:
+  print "Need to pass a URL!"
+  exit()
 
 # which pages have we done already? (so that we don't do them twice)
 done       = [url]
@@ -27,7 +52,7 @@ pages      = [url]
 # what pages have matched our search text?
 results    = []
 
-for i in range(0, depth):
+for level in range(1, depth + 1):
   # collect the pages we should look at in the next iteration
   next_pages = []
   for url in pages:
@@ -41,7 +66,7 @@ for i in range(0, depth):
       if page == None:
         page = "/"
 
-      print "searching %s, page %s" % (url, page)
+      #print "searching %s, page %s" % (url, page)
 
       try:
         # request the page
@@ -50,11 +75,11 @@ for i in range(0, depth):
         resp = http.getresponse()
         data = resp.read()
 
-        print "Got it, parsing time"
-
-        # check to see if this page has our search text
-        if data.lower().find(search_text.lower()) >= 0:
-          results.append(url + "/" + page)
+        # count the number of times this document matches our searches
+        matches = [word.lower() for word in search_texts if word.lower() in data.lower()]
+        if len(matches) > 0:
+          # keep track of the URL, the level, and the number of matches
+          results.append((url + "/" + page, level, len(matches)))
 
         soup = BeautifulSoup(data)
 
@@ -71,22 +96,31 @@ for i in range(0, depth):
 
             # don't look at it if we've already seen it
             if href not in done:
-              print "Found link: %s" % href
-              next_pages.append(href)
-              done.append(href)
+              # apply URL filter unless we don't have one
+              if url_filter == None or unicode(url_filter).lower() in unicode(link.contents[0]).lower():
+                #print "Found link: %s" % href
+                next_pages.append(href)
+                done.append(href)
       except httplib.BadStatusLine:
         # no worries, just ignore it
         print "bad status on %s, %s" % (url, page)
+        pass
       except HTMLParseError:
         # same deal
+        print "parse error on %s, %s" % (url, page)
         pass
       except httplib.socket.timeout:
         pass
       except httplib.socket.gaierror:
         pass
     else:
-      print "Invalid URL: %s" % url
+      #print "Invalid URL: %s" % url
+      pass
 
   pages = next_pages
 
-print results
+for result in results:
+  if result in blacklist:
+    print "Blacklisted: (%s, level %d, matches %d)" % result
+  else:
+    print "(%s, level %d, matches %d)" % result
